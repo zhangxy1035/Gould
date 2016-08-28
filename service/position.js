c8e752650e7c7644ebe79e39b44842c360ce4739
cancer-PC\cancer <zhangxy1035@gmail.com>
2016-08-28 17:47:01 +0800

    service
    使用node作为后台服务

---
+++ service/position.js
@@ -0,0 +1,63 @@
+/**
+ * Created by David on 2016-08-17.
+ */
+//var sql = require('mssql');
+var sql = require('mysql');
+var connection = sql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'zhang123',
+    database:'cainiao'
+});
+connection.connect();
+/**
+ * 获取所有标记点
+ * @param req
+ * @param res
+ */
+exports.getAllMarker = function(req,res) {
+    console.log('获取所有标记点服务器端');
+    console.log(req.body);
+
+    var con = connection.query("select * from new_1",function(err,result,fields){
+        if(err){
+            throw err;
+        }
+        console.log("rows",result[0].site_id);
+        var new_1 = [];
+        result.forEach(function (item) {
+            var new_1item = {
+                site_id: item.site_id,
+                lng: item.lng,
+                lat: item.lat
+            };
+            new_1.push(new_1item);
+        });
+        console.log("rowsss",new_1);
+        res.send({retCode:1,data:new_1});
+    })
+
+};
+//网点
+exports.getAllMarker2 = function(req,res) {
+    console.log('获取所有标记点服务器端');
+    console.log(req.body);
+
+    var con = connection.query("select * from new_2",function(err,result,fields){
+        if(err){
+            throw err;
+        }
+        var new_2 = [];
+        result.forEach(function (item) {
+            var new_1item = {
+                spot_id: item.spot_id,
+                lng: item.lng,
+                lat: item.lat
+            };
+            new_1.push(new_1item);
+        });
+        console.log("rowsss",new_2);
+        res.send({retCode:1,data:new_2});
+    })
+
+};
